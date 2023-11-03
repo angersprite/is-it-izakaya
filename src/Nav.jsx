05223106751a65f624b432477ee0719278a11957
angersprite <ktlankton@gmail.com>
2023-11-03 12:05:56 -0500

    initial commit

---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+export default function MediaDisplay(){
+    return (
+        <div>
+            <h2>media title</h2>
+            <div>
+                <img src="" alt="media image" />
+            </div>
+            <p>media description</p>
+        </div>
+    )
+}
