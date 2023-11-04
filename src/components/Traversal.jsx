b22dcb0614f1fc5429aec49b2f9ceb1fcae4e13b
angersprite <ktlankton@gmail.com>
2023-11-03 21:35:43 -0500

    phone styling

--- src/components/Traversal.jsx
+++ src/components/Traversal.jsx
@@ -3,8 +3,8 @@ import '../styles/traversal.css'
 export default function TraversalButtons() {
     return (
         <div id="traversal-container">
-            <button className="traversal-button">🡄</button>
-            <button className="traversal-button">🡆</button>
+            <button className="traversal-button">&lt;</button>
+            <button className="traversal-button">&gt;</button>
         </div>
     )
 }
